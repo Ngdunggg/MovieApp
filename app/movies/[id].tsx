48a3d6fe38162f700170b91716257775403d6263
Ngdunggg <momcon150804@gmail.com>
2025-03-16 00:52:20 +0700

    First commit with build project

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import Reat from 'react';
+
+const MovieDetail = () => {
+    return (
+        <View>
+            <Text>Movie Detail Screen</Text>
+        </View>
+    )
+}
+
+export default MovieDetail;
